8f1e93f201422cc66e1eb5f2b87d93601731b3bd
KpSantiago <santiago.kauajose11@gmail.com>
2023-01-12 20:28:57 -0300

    commit updated

--- functions/src/config/database.js
+++ functions/src/config/database.js
@@ -5,6 +5,7 @@ module.exports = {
 	host: "containers-us-west-25.railway.app",
 	port: "7229",
 	password: "NHjrn7mpa7ZmukWqH38a",
+	url: process.env.DATABASE_URL,
 	define: {
 		timestamp: false,
 		underscored: true,
