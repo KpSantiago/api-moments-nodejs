5f5f5ed763792bd0eeb679cbfe48937f34521d1e
KpSantiago <santiago.kauajose11@gmail.com>
2023-09-15 12:09:59 -0300

    updates added

--- functions/src/config/database.js
+++ functions/src/config/database.js
@@ -1,13 +1,9 @@
 module.exports = {
-	database: "railway",
-	username: "root",
-	dialect: "mysql",
-	host: "containers-us-west-25.railway.app",
-	port: "7229",
-	password: "NHjrn7mpa7ZmukWqH38a",
-	url: process.env.DATABASE_URL,
-	define: {
-		timestamp: false,
-		underscored: true,
-	},
+	database: "verceldb",
+	username: "default",
+	dialect: "postgres",
+	host: "ep-odd-lab-34556090.us-east-1.postgres.vercel-storage.com",
+	sslmode: "verify-full",
+	password: "vdk3Uj1bluRw",
+	url: "postgres://default:vdk3Uj1bluRw@ep-odd-lab-34556090.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=verify-full",
 };
